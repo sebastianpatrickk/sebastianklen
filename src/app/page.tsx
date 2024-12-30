fc19fffd2c518b061209e0a0eb09adea082c08c9
Sebastian Patrick Klen <sebastianpatrickklen@gmail.com>
2024-12-30 21:09:01 +0100

    Update homepage message to indicate website is under construction

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,8 @@ export default function Home() {
   return (
     <div className="w-full min-h-screen flex items-center justify-center">
       <p className="px-4 h-10 text-[#171717] rounded-full flex items-center border border-[#EBEBEB] bg-white font-medium">
-        sebastianklen.com
+        🚧 This website is under construction
+        <span className="ml-2 animate-bounce">🔧✨</span>🚧
       </p>
     </div>
   );
