22d24392ca42951cbcadd6c6668f96db29f43b8a
Sebastian Patrick Klen <sebastianpatrickklen@gmail.com>
2024-12-31 19:40:38 +0100

    Fix punctuation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,16 +5,16 @@ export default function Home() {
   return (
     <div className="flex w-full flex-col gap-6">
       <p>
-        Hi, I'm Sebastian, full-stack developer who loves building and improving
-        things. <br />
-        I'm currently working at{" "}
+        Hi, I&apos;m Sebastian, full-stack developer who loves building and
+        improving things. <br />
+        I&apos;m currently working at{" "}
         <Link
           href="https://www.taskgrip.com/en"
           className="text-primary-foreground hover:text-primary"
         >
           TaskGrip
         </Link>
-        , but I'm looking for new opportunities.
+        , but I&apos;m looking for new opportunities.
       </p>
       <div className="space-y-3">
         <h1 className="font-medium">Experience</h1>
