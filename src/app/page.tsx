bc93f66d303de81b96d5c3b213fe09110aacdb6f
Sebastian Patrick Klen <sebastianpatrickklen@gmail.com>
2024-12-31 18:29:08 +0100

    Update Home component and add dynamic PostPage component for blog posts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,3 @@
 export default function Home() {
-  return (
-    <div className="w-full min-h-screen flex items-center justify-center">
-      <p className="px-4 h-10 text-[#171717] rounded-full flex items-center border border-[#EBEBEB] bg-white font-medium">
-        🚧 This website is under construction
-        <span className="ml-2 animate-bounce">🔧✨</span>🚧
-      </p>
-    </div>
-  );
+  return <div className="w-full"> Website content</div>;
 }
