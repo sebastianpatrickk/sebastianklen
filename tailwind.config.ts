d6aa922367725ffc7fcd3a274b6a74263f47fa96
Sebastian Patrick Klen <sebastianpatrickklen@gmail.com>
2024-12-31 18:27:49 +0100

    Add CSS variables for theming and extend Tailwind configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,9 +8,19 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["var(--font-geist-sans)"],
+        mono: ["var(--font-geist-mono)"],
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+
+        primary: "var(--primary)",
+        "primary-foreground": "var(--primary-foreground)",
+
+        text: "var(--text)",
+        "text-muted": "var(--text-muted)",
       },
     },
   },
