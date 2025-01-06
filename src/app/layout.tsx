8ec8dd3f75850d9aa44f62f150d318f15671b848
Sebastian Patrick Klen <sebastianpatrickklen@gmail.com>
2025-01-06 17:48:17 +0100

    Migrate from geist font to inter

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,13 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const inter = Inter({
+  variable: "--font-inter",
+  display: "swap",
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
@@ -33,13 +28,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
-      <body className="font-mono antialiased">
+    <html lang="en" className={` ${inter.variable}`}>
+      <body className="font-sans antialiased">
         <div className="relative flex min-h-screen flex-col justify-between bg-background p-6 pt-0 text-text md:p-8">
-          <div className="pointer-events-none fixed left-0 top-0 z-50 h-6 w-full bg-gradient-to-b from-background to-transparent" />
+          <div className="bg-fade-to-transparent pointer-events-none fixed left-0 top-0 z-50 h-14 w-full" />
           <div>
             <Header />
-            <div className="mx-auto mt-12 w-full max-w-2xl">{children}</div>
+            <div className="mx-auto mt-6 w-full max-w-screen-sm text-sm">
+              {children}
+            </div>
           </div>
           <Footer />
         </div>
