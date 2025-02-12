30b83df6f045a83756ca32d02e1951ff0585a58e
Sebastian Patrick Klen <sebastianpatrickklen@gmail.com>
2025-02-12 19:47:46 +0100

    Fix tracking script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,10 +34,10 @@ export default function RootLayout({
     <html lang="en" className={`${inter.variable}`}>
       <body className="relative min-h-screen font-sans antialiased">
         <Script
-          defer
           src="https://assets.onedollarstats.com/stonks.js"
+          strategy="afterInteractive"
           id="stonks"
-        ></Script>
+        />
         <div className="pointer-events-none fixed left-0 top-0 z-50 h-8 w-full bg-fade-to-transparent md:h-14" />
         <div className="mx-auto max-w-screen-sm px-6 pt-8 md:pt-14">
           <Header />
