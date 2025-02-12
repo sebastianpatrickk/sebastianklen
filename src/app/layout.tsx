96e13ad3c7831cfab57c85eb5b5a766d8df2d876
Sebastian Patrick Klen <sebastianpatrickklen@gmail.com>
2025-02-12 19:44:00 +0100

    Add analytics script to root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
+import Script from "next/script";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -32,6 +33,11 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${inter.variable}`}>
       <body className="relative min-h-screen font-sans antialiased">
+        <Script
+          defer
+          src="https://assets.onedollarstats.com/stonks.js"
+          id="stonks"
+        ></Script>
         <div className="pointer-events-none fixed left-0 top-0 z-50 h-8 w-full bg-fade-to-transparent md:h-14" />
         <div className="mx-auto max-w-screen-sm px-6 pt-8 md:pt-14">
           <Header />
